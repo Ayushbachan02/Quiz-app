3114c37525108c494821abbc5c449a66778e201c
Ayushbachan02 <bachanayush@gmail.com>
2024-10-28 11:58:52 +0530

    Added HTML structure for quiz app, CSS styles for layout and design

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
 console.log("hello")
+
+const question = [
+    {
+        question: "Which is the biggest animal among them?",
+        answer: [
+            {text: "Tiger" , correct:false},
+            {text: "Elephant" , correct:false},
+            {text: "Blue Whale" , correct:true},
+            {text: "Hippo" , correct:false},
+
+        ]
+    },
+
+    {
+        question: "Which is the smallest continent in the world?",
+        answer: [
+            {text: "Austrila" , correct:true},
+            {text: "Asia" , correct:false},
+            {text: "Arctic" , correct:false},
+            {text: "Africa" , correct:false},
+
+        ]
+    },
+
+    {
+        question: "which is the largest desert in the world?",
+        answer: [
+            {text: "Sahara" , correct:false},
+            {text: "Gobi" , correct:false},
+            {text: "Kalahari" , correct:false},
+            {text: "Antarctica" , correct:true},
+
+        ]
+    },
+
+    {
+        question: "which is the smallest country in the world?",
+        answer: [
+            {text: "vatican city" , correct:true},
+            {text: "bhutan" , correct:false},
+            {text: "Nepal" , correct:false},
+            {text: "Sri Lanka" , correct:false},
+
+        ]
+    },
+
+];
+
+const questionEle = document.queryselectorbyId("question");
+const answerbtn = document.queryselectorbyId("answer-btn");
+const nextbtn = document.queryselectorbyId("next-btn");
+
+
+let currentIndex = 0;
+let score =0;
+
+function startQuiz(){
+    currentIndex = 0;
+    score = 0;
+    nextbtn.innerHTML = "Next"
+}
